48c3f6f76692d8a198d75f7a96dec11c5c4ac890
marissann11 <wilhite.marissa@gmail.com>
2022-04-11 02:42:31 -0400

    minor changes with themes

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import pic from '../assets/images/pic.png';
+import { FaAngleDoubleDown } from 'react-icons/fa';
 
 const About = () => {
   return (
@@ -10,6 +11,7 @@ const About = () => {
       <div className="about-text">
         <h1>Hello!</h1>
         <p>Things about me I guess</p>
+        <FaAngleDoubleDown />
       </div>
     </div>
   );
