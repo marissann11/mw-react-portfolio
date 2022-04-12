5c48b4a135291008a068953047d059f43008d946
marissann11 <wilhite.marissa@gmail.com>
2022-04-12 09:14:10 -0400

    update to theme and about comp

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,16 @@ import { FaSun, FaCloudMoon } from 'react-icons/fa';
 const About = () => {
   return (
     <div className="about row">
+      <div className="wrap">
+        <div className="pic"></div>
+      </div>
       <div className="about-text">
         <h1>Hello!</h1>
         <p>
           This will be where I write all the things. <br />
           This will be where is will be wI write all the things. <br />
-          This will be whhere is will be ere I write all tis will be where I
-          write all the thingsdfxcgvhbjnk. <br />
-          This will be whhere is will be ere I write all the things. <br />
+          This w This will be whhere is will be ere I write all the things.{' '}
+          <br />
           This will be where I write all the will be where I write all the
           things. <br /> <br />
           This will be where I write all the things. <br />
@@ -27,11 +29,6 @@ const About = () => {
           <FaCloudMoon />
         </div>
       </div>
-      <div className="wrap">
-        <div className="circle"></div>
-        <div className="pic"></div>
-      </div>
-      <div className="overlay"></div>
     </div>
   );
 };
