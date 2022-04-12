d9fc2c223d33e16a27f94058d47ece1b88c9b773
marissann11 <wilhite.marissa@gmail.com>
2022-04-12 02:35:35 -0400

    refactor of about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import me from '../assets/images/me.png';
 
 const About = () => {
   return (
-    <div className="about">
-      <img src={me} alt={`me`} className="my-img"></img>
-      <div className="overlay"></div>
-      <div className="greeting"></div>
+    <div className="about row">
+      <div className="circle"></div>
+      <div className="pic"></div>
+      <div className="about-text">
+        <h1>Hello</h1>
+        <p>
+          This will be where I write all the things. <br />
+          This will be where is will be wI write all the things. <br />
+          This will be whhere is will be ere I write all tis will be where I
+          write all the things. <br />
+          This will be whhere is will be ere I write all the things. <br />
+          This will be where I write all the will be where I write all the
+          things. <br /> <br />
+          This will be where I write all the things. <br />
+          This will be where is will be wI write all the things. <br />
+          This will be whhere is will be ere I write all tis will be where I
+          write all the things. <br />
+          This will be whhere is will be ere I write all the things. <br />
+          This will be where I write all the will be where I write all the
+          things. <br />
+        </p>
+      </div>
     </div>
   );
 };
