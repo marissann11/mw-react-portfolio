bd6682e7fcfd07c034ca8592f344941b5857afb4
marissann11 <wilhite.marissa@gmail.com>
2022-04-13 11:56:43 -0400

    added containers for skill component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import '../styles/skills.scss';
+import { FaReact, FaJsSquare, FaNodeJs } from 'react-icons/fa';
 
 const Skills = () => {
-  return <section className="skills"></section>;
+  return (
+    <section className="skills">
+      <div className="container">
+        <div className="top">
+          <FaJsSquare />
+        </div>
+        <h2>Title</h2>
+        <p>This is there I would write things about this thing I can do</p>
+      </div>
+      <div className="container">
+        <div className="top">
+          <FaNodeJs />
+        </div>
+        <h2>Title</h2>
+        <p>This is there I would write things about this thing I can do</p>
+      </div>
+      <div className="container">
+        <div className="top">
+          <FaReact />
+        </div>
+        <h2>Title</h2>
+        <p>This is there I would write things about this thing I can do</p>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
