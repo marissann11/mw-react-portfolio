95c5d8faf224953c1cbd3b11ef12cac93aee9406
marissann11 <wilhite.marissa@gmail.com>
2022-04-17 00:50:10 -0400

    updated skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import './skills.scss';
-import { FaReact, FaJsSquare, FaNodeJs, FaCodeBranch } from 'react-icons/fa';
+import { FaReact, FaNodeJs, FaCodeBranch } from 'react-icons/fa';
 
 const Skills = () => {
   return (
     <section className="skills">
       <div className="container">
         <div className="top">
-          <FaJsSquare />
+          <FaReact />
         </div>
-        <h2>Title</h2>
-        <p>This is there I would write things about this thing I can do</p>
+        <h2>Frontend Technologies</h2>
+        <p>
+          JavaScript <br />
+          React.js <br />
+          jQuery <br />
+          HTML5 <br />
+          CSS <br />
+          Bootstrap <br />
+        </p>
       </div>
       <div className="container">
         <div className="top">
           <FaNodeJs />
         </div>
-        <h2>Title</h2>
-        <p>This is there I would write things about this thing I can do</p>
-      </div>
-      <div className="container">
-        <div className="top">
-          <FaReact />
-        </div>
-        <h2>Title</h2>
-        <p>This is there I would write things about this thing I can do</p>
+        <h2>Backend Technologies</h2>
+        <p>
+          Node.js <br />
+          Express.js <br />
+          MERN Stack <br />
+          MySql <br />
+          User Auth <br />
+          Session Storage
+        </p>
       </div>
       <div className="container">
         <div className="top">
           <FaCodeBranch />
         </div>
-        <h2>Title</h2>
-        <p>This is there I would write things about this thing I can do</p>
+        <h2>Deployment & Version Control</h2>
+        <p>
+          Heroku <br />
+          Github <br />
+          MongoDB Atlas
+        </p>
+      </div>
+      <div className="container">
+        <div className="top"></div>
+        <h2>Resume</h2>
+        <p>Click to download my Resume!</p>
       </div>
     </section>
   );
